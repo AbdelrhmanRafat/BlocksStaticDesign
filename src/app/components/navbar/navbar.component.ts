eb5c6e1d6a32f5895496b0cb1ec69b4bd47b3900
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2025-06-20 14:52:30 +0300

    Add Diffrent Layouts for Product Features.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule, NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isMenuOpen = false;
 
+  navigationItems = [
+    { path: '/home', label: 'Home' },
+    { path: '/product-features', label: 'Product Features' },
+    { path: '/faq', label: 'FAQ' }
+  ];
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
